72dcf3237cfe53cfcc8fd2a04352f977d3988efb
Jesse Connell <jesse08@gmail.com>
2016-10-11 20:11:31 -0400

    common bash_profile settings

--- setup.sh
+++ setup.sh
@@ -10,5 +10,7 @@ done
 
 [ -f ~/.xscreensaver ] || cp "$DIRPATH/xscreensaver" ~/.xscreensaver
 
+bash_profile_line="source ~/$DIR/bash_profile.common # Shared settings"
 bashrc_line="source ~/$DIR/bashrc.common # Shared settings"
+grep -q -s -F "$bash_profile_line" ~/.bash_profile || echo "$bash_profile_line" >> ~/.bash_profile
 grep -q -s -F "$bashrc_line" ~/.bashrc || echo "$bashrc_line" >> ~/.bashrc
